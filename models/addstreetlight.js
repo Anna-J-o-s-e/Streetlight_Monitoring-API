e6311a88533edaa06877175bc9ea37fbe2243fb0
Anna-J-o-s-e <annajose941@gmail.com>
2024-10-12 12:58:05 +0530

    iot integration  completed

--- models/addstreetlight.js
+++ models/addstreetlight.js
@@ -1,9 +1,12 @@
-const mongoose=require("mongoose")
-const schema=mongoose.Schema(
+const mongoose = require("mongoose")
+const schema = mongoose.Schema(
     {
+        "id": { type: String, required: true, unique: true },
+        "status": { type: String, required: true },
+        "thingspeakChannelId": { type: String, required: true },
         "location": { type: String, required: true },
-        "date": { type: String, required: true },
+        "date": { type: Date, required: false }
     }
 )
-let addstreetlightsmodel=mongoose.model("addstreetlights",schema)
-module.exports={addstreetlightsmodel}
+let addstreetlightsmodel = mongoose.model("addstreetlights", schema)
+module.exports = { addstreetlightsmodel }
